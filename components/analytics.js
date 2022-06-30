7979ba6eb85081d2ab341aced8b663c4f6c268bd
Eric Turner <eric@ericturner.dev>
2022-06-30 12:50:00 -0400

    Remove incorrect comment from Analytics component

--- components/analytics.js
+++ components/analytics.js
@@ -2,8 +2,6 @@ import Script from "next/script"
 import Config from '../lib/config';
 
 export default function Analytics() {
-    // Note: It appears that Cloudflare is only recording visits (i.e. when someone navigates to your
-    // website directly or from an external referrer). Navigation within a site is not being tracked.
     const src = 'https://static.cloudflareinsights.com/beacon.min.js';
     return (
         <Script
