317628559e772c1c7fdd6cd5a4d4b611d1ce8ea6
Eric Turner <eric@ericturner.dev>
2022-07-01 10:18:20 -0400

    Add Disqus commenting system to blog posts

--- components/post.js
+++ components/post.js
@@ -1,6 +1,8 @@
-import { getMDXComponent } from 'mdx-bundler/client';
-import { useMemo } from 'react';
+import {getMDXComponent} from 'mdx-bundler/client';
+import {useMemo} from 'react';
 import Date from './date';
+import {DiscussionEmbed} from "disqus-react";
+import Config from "../lib/config";
 
 import styles from './post.module.css';
 import SyntaxHighlighter from "./syntaxHighlighter";
@@ -8,7 +10,7 @@ import classNames from "classnames";
 import Back from "./back";
 import Layout from './layout';
 
-export default function Post({frontmatter, code, className}) {
+export default function Post({slug, frontmatter, code, className}) {
     const Component = useMemo(() => getMDXComponent(code), [code]);
 
     return (
@@ -25,12 +27,25 @@ export default function Post({frontmatter, code, className}) {
                         <Component components={{
                             Date,
                             pre: SyntaxHighlighter,
-                        }} />
+                        }}/>
                     </div>
                 </article>
             </main>
 
-            <Back href="/" label="home" />
+            <Back href="/" label="home"/>
+
+            <DiscussionEmbed
+                className='mt-4 px-6 md:mt-8 md:px-10'
+                shortname={Config.disqus.shortname}
+                config={
+                    {
+                        url: `${Config.url}/posts/${slug}`,
+                        identifier: slug,
+                        title: frontmatter.title,
+                        language: 'en_US'
+                    }
+                }
+            />
 
             <footer className="mb-10">
             </footer>
