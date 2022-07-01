bd11428a48b0e83b54e870684d272a8d1de02134
Eric Turner <eric@ericturner.dev>
2022-07-01 13:45:49 -0400

    Add profile pic to home page

--- components/home.js
+++ components/home.js
@@ -2,21 +2,32 @@ import Posts from './posts';
 import Link from 'next/link';
 import classNames from 'classnames';
 import Layout from './layout';
+import Image from "next/image";
 
-export default function Home({ allPostsData, className}) {
+export default function Home({allPostsData, className}) {
     const siteTitle = 'Home - ericturner.dev';
     const maxPosts = 10;
 
     return (
         <Layout siteTitle={siteTitle} className={classNames('home', className)}>
             <section className="hero flex flex-col items-center py-6 bg-blue-200 dark:bg-slate-700 md:py-10">
-                <h1>Hi, I&apos;m Eric</h1>
-                <h2>Welcome to my site</h2>
+                <div className="flex flex-row items-center">
+                    <Image
+                        className="rounded-lg"
+                        src="/images/profile.jpg"
+                        width={384/4}
+                        height={466/4}
+                    />
+                    <div className="flex flex-col items-start ml-5">
+                        <h1>Hi, I&apos;m Eric</h1>
+                        <h2>Welcome to my site</h2>
+                    </div>
+                </div>
             </section>
 
             <section className="blog-posts mt-4 px-6 md:px-10 md:mt-8">
                 <h2>Blog Posts</h2>
-                <Posts className="mt-5" postsData={allPostsData.slice(0, maxPosts)} />
+                <Posts className="mt-5" postsData={allPostsData.slice(0, maxPosts)}/>
                 <Link href="/blog"><a className="block font-semibold mt-8">Read all blog posts</a></Link>
             </section>
 
