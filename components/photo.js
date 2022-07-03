55124077a403360fce0eed4ee8277edbf30c59c1
Eric Turner <eric@ericturner.dev>
2022-07-03 11:23:16 -0400

    Move photo attribution over the photo

--- components/photo.js
+++ components/photo.js
@@ -5,17 +5,20 @@ import classNames from "classnames";
 export default function Photo({src, alt, width, height, photographerName, photographerUrl, className}) {
     return (
         <div className={classNames(`photo`, className)}>
-            <Image width={width ? width : 400}
-                   height={height ? height : 400}
-                   src={src}
-                   alt={alt ? alt : 'Missing a description of the photo'}
-                    className="rounded-lg" />
-            {
-                photographerName &&
-                <p className="text-sm font-extralight">
-                    Photo by {photographerUrl ? <ExternalLink className="text-slate-800 dark:text-slate-300" href={photographerUrl}>{photographerName}</ExternalLink> : photographerName}
-                </p>
-            }
+            <div className="relative">
+                <Image width={width ? width : 400}
+                       height={height ? height : 400}
+                       src={src}
+                       alt={alt ? alt : 'Missing a description of the photo'}
+                       className="rounded-lg"/>
+                {
+                    photographerName &&
+                    <p className="text-sm absolute bottom-0 pl-2">
+                        Photo by {photographerUrl ? <ExternalLink className="text-slate-800 dark:text-slate-300 font-semibold"
+                                                                  href={photographerUrl}>{photographerName}</ExternalLink> : photographerName}
+                    </p>
+                }
+            </div>
         </div>
     )
 }
