2852868d5b1457a26b8dbecbabbcf939001477fd
Eric Turner <eric@ericturner.dev>
2022-07-03 12:10:40 -0400

    Fix problem with items on blog page not being centered.

--- components/blog.js
+++ components/blog.js
@@ -8,13 +8,13 @@ export default function Blog({postsData, categories, className}) {
 
     return (
         <Layout siteTitle={siteTitle} className={classNames('blog', className)}>
-            <section className="flex flex-col items-center max-w-6xl">
+            <section className="flex flex-col items-center">
                 <div className="flex flex-row flex-wrap max-w-6xl px-5 mt-5">
                         {categories && categories.map(category => <CategoryLink key={category}>{category}</CategoryLink>)}
                 </div>
             </section>
 
-            <section className="flex flex-col items-center max-w-6xl">
+            <section className="flex flex-col items-center">
                 <div className="max-w-6xl px-5">
                     <Posts className="mt-5" postsData={postsData}/>
                 </div>
