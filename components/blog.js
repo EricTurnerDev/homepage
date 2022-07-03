40483c5a56c0b82106e198ac9ef2276998a0b7a5
Eric Turner <eric@ericturner.dev>
2022-07-03 00:43:35 -0400

    Fix alignment of categories on blog page.

--- components/blog.js
+++ components/blog.js
@@ -8,7 +8,7 @@ export default function Blog({postsData, categories, className}) {
 
     return (
         <Layout siteTitle={siteTitle} className={classNames('blog', className)}>
-            <section className="flex flex-col">
+            <section className="flex flex-col items-center">
                 <div className="flex flex-row flex-wrap max-w-6xl px-5 mt-5">
                         {categories && categories.map(category => <CategoryLink key={category}>{category}</CategoryLink>)}
                 </div>
