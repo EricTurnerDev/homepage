b2f52dbcdea23bae6790aacdb4320ebf892b8f5a
Eric <eric@ericturner.dev>
2022-07-26 14:49:13 -0700

    Make footer show date range for copyright date

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,10 +6,13 @@ interface FooterProps {
 }
 
 export default function Footer({className}: FooterProps) {
+    const startYear = 2022;
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className={classNames("footer mt-10", className)}>
             <div className="flex flex-col items-center">
-                <p>Copyright &copy; {new Date().getFullYear()}, {Config.user.firstName} {Config.user.surname}</p>
+                <p>Copyright &copy; {currentYear > startYear ? `${startYear}-${currentYear}` : startYear}, {Config.user.firstName} {Config.user.surname}</p>
             </div>
         </footer>
     )
